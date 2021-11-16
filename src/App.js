2a704ade5bd9fba654092992f065d1eee33f0f7a
mu41555 <usama41555@gmail.com>
2021-11-16 04:29:52 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { NavLink } from "react-router-dom";
 
 function App() {
+  const [covid, setcovid] = useState([]);
+  const api = axios.create({
+    baseURL: "https://api.covidtracking.com/v1/states/current.json",
+  });
+  const getdata = () => {
+    api.get().then((e) => {
+      setcovid(e.data);
+    });
+  };
+
+  useEffect(() => {
+    getdata();
+  }, []);
+
+  const details = (e) => {
+    console.log(e);
+    // const location = useLocation();
+    // console.log(location.state);
+    // const navigate = useNavigate();
+    // let obj = {
+ 
+    // };
+    // navigate(`/details/${obj}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3 className="container">Covid Tracker</h3>
+      <table className="container">
+        <thead>
+          <tr className="head">
+            <td>S no.</td>
+            <td>checkTimeEt</td>
+            <td>positive</td>
+            <td>negative</td>
+            <td>hospitalized</td>
+            <td>totalTestsViral</td>
+            <td>totalTestResults</td>
+            <td>onVentilatorCurrently</td>
+            <td>total</td>
+          </tr>
+        </thead>
+        <tbody>
+          {covid.map((detail, index) => (
+            <tr
+              key={index}
+              onClick={() => {
+                details(detail);
+              }}
+            >
+              <td>{index + 1}</td>
+              <td>{detail.checkTimeEt}</td>
+              <td>{detail.positive}</td>
+              <td>{detail.negative}</td>
+              <td>{detail.hospitalized}</td>
+              <td>{detail.totalTestsViral}</td>
+              <td>{detail.totalTestResults}</td>
+              <td>{detail.onVentilatorCurrently}</td>
+              <td>{detail.total}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
